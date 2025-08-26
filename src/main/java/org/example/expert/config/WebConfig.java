7d84201c3c1524b83f01244222087b27258c1ba9
abko <abko121115@gmail.com>
2025-08-26 16:37:23 +0900

    refactor: 필요 없는 `@RequiredArgsConstructor` 제거

--- src/main/java/org/example/expert/config/WebConfig.java
+++ src/main/java/org/example/expert/config/WebConfig.java
@@ -1,13 +1,11 @@
 package org.example.expert.config;
 
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
