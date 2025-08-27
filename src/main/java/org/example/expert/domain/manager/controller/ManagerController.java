fef9e252f7ad954108503e12700b3b1f83762dcd
abko <abko121115@gmail.com>
2025-08-27 19:59:54 +0900

    refactor: JwtUtil 관련 코드 제거 및 `@AuthUser` 사용하도록 개선

--- src/main/java/org/example/expert/domain/manager/controller/ManagerController.java
+++ src/main/java/org/example/expert/domain/manager/controller/ManagerController.java
@@ -1,9 +1,7 @@
 package org.example.expert.domain.manager.controller;
 
-import io.jsonwebtoken.Claims;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.example.expert.config.JwtUtil;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.manager.dto.request.ManagerSaveRequest;
@@ -20,14 +18,10 @@ import java.util.List;
 public class ManagerController {
 
     private final ManagerService managerService;
-    private final JwtUtil jwtUtil;
 
     @PostMapping("/todos/{todoId}/managers")
-    public ResponseEntity<ManagerSaveResponse> saveManager(
-            @Auth AuthUser authUser,
-            @PathVariable long todoId,
-            @Valid @RequestBody ManagerSaveRequest managerSaveRequest
-    ) {
+    public ResponseEntity<ManagerSaveResponse> saveManager(@Auth AuthUser authUser,
+            @PathVariable long todoId, @Valid @RequestBody ManagerSaveRequest managerSaveRequest) {
         return ResponseEntity.ok(managerService.saveManager(authUser, todoId, managerSaveRequest));
     }
 
@@ -37,13 +31,8 @@ public class ManagerController {
     }
 
     @DeleteMapping("/todos/{todoId}/managers/{managerId}")
-    public void deleteManager(
-            @RequestHeader("Authorization") String bearerToken,
-            @PathVariable long todoId,
-            @PathVariable long managerId
-    ) {
-        Claims claims = jwtUtil.extractClaims(bearerToken.substring(7));
-        long userId = Long.parseLong(claims.getSubject());
-        managerService.deleteManager(userId, todoId, managerId);
+    public void deleteManager(@Auth AuthUser user, @PathVariable long todoId,
+            @PathVariable long managerId) {
+        managerService.deleteManager(user.getId(), todoId, managerId);
     }
 }
